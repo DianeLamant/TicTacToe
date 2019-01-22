99abc32af644b278d9388e12a3fce20beafd0e6c
DianeLam <diane.lamant.simplon@gmail.com>
2019-01-22 18:13:52 +0100

    utilisation matrice

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ let etatBox = [vide, croix, rond];
 let player1 = 1;
 let player2 = 2;
 let players = [player1, player2];
-let currentPlayer = player1;
+let currentPlayer = player2;
 let joueurActuel = document.getElementById("player");
 let winner = document.getElementById("gagnant");
 let textFinal = document.getElementById("winnerText");
@@ -20,115 +20,76 @@ let matrice = [
 
 // Indique a quel joueur c'est de jouer
 
-function player() {
-    if (currentPlayer == player1) {
-        joueurActuel.src = croix;
-    } else if (currentPlayer == player2) {
-        joueurActuel.src = rond;
-    }
-}
-player();
-
-// Permet de changer de joueur après avoir jouer
-
 function changementJoueur() {
-    if (currentPlayer == player1) {
-        currentPlayer = player2;
-    } else {
+    if (currentPlayer == player2) {
+        joueurActuel.src = croix;
         currentPlayer = player1;
+    } else if (currentPlayer == player1) {
+        joueurActuel.src = rond;
+        currentPlayer = player2;
     }
 }
+changementJoueur();
 
 // Si la case est vide, elle se remplit suivant le joueur
 
 for (i = 0; i < allCases.length; i++) {
     allCases[i].onclick = function (event) {
         animation(event.target);
-        console.log(matrice);
-        if (event.target.src.endsWith(vide) && currentPlayer == player1) {
+        let x = event.target.dataset.x;
+        let y = event.target.dataset.y;
+        if (matrice[x][y] == 0 && currentPlayer == player1) {
+            matrice[x][y] = 1;
             event.target.src = croix;
-            matriceP1();
             changementJoueur();
-            player();
-        } else if (event.target.src.endsWith(vide) && currentPlayer == player2) {
+            win(croix, player1);
+        } else if (matrice[x][y] == 0 && currentPlayer == player2) {
+            matrice[x][y] = 2;
             event.target.src = rond;
-            matriceP2();
             changementJoueur();
-            player();
+            win(rond, player2);
         } else {
             alert("La case est déjà prise !");
         }
-        // win(croix);
-        // win(rond);
-    }
-}
 
-function matriceP1() {
-    for (x = 0; x < matrice.length; x++) {
-        for (y = 0; y < matrice[x].length; y++) {
-            matrice[x][y] = player1;
-        }
-    }
-}
-
-
-function matriceP2() {
-    for (x = 0; x < matrice.length; x++) {
-        for (y = 0; y < matrice[x].length; y++) {
-            matrice[x][y] = player2;
-        }
     }
 }
 
 // Fonction qui vérifie les combinaisons gagnantes
 
-function win() {
-    // console.log(matrice);
-    for (let player of players) {
-        if (
-            // Colonnes
-            (matrice[0][0] === player && matrice[1][0] === player && matrice[2][0] === player) ||
-            (matrice[0][1] === player && matrice[1][1] === player && matrice[2][1] === player) ||
-            (matrice[0][2] === player && matrice[1][2] === player && matrice[2][2] === player) ||
-            // Lignes
-            (matrice[0][0] === player && matrice[0][1] === player && matrice[0][2] === player) ||
-            (matrice[1][0] === player && matrice[1][1] === player && matrice[1][2] === player) ||
-            (matrice[2][0] === player && matrice[2][1] === player && matrice[2][2] === player) ||
-            // Diagonales
-            (matrice[0][2] === player && matrice[1][1] === player && matrice[2][0] === player) ||
-            (matrice[0][0] === player && matrice[1][1] === player && matrice[2][2] === player)
-        ) {
-            winner.src = player;
+function win(gagnant, player) {
+    if (
+        // Colonnes
+        (matrice[0][0] === player && matrice[1][0] === player && matrice[2][0] === player) ||
+        (matrice[0][1] === player && matrice[1][1] === player && matrice[2][1] === player) ||
+        (matrice[0][2] === player && matrice[1][2] === player && matrice[2][2] === player) ||
+        // Lignes
+        (matrice[0][0] === player && matrice[0][1] === player && matrice[0][2] === player) ||
+        (matrice[1][0] === player && matrice[1][1] === player && matrice[1][2] === player) ||
+        (matrice[2][0] === player && matrice[2][1] === player && matrice[2][2] === player) ||
+        // Diagonales
+        (matrice[0][2] === player && matrice[1][1] === player && matrice[2][0] === player) ||
+        (matrice[0][0] === player && matrice[1][1] === player && matrice[2][2] === player)
+    ) {
+        winner.src = gagnant;
+        end();
+    } else {
+        if (matrice.every(equality)) {
+            textFinal.innerHTML = "Egalité !";
             end();
         }
     }
 }
 
-// function win(etatBoxX) {
-//     if (
-//         // Colonnes
-//         (allCases[0].src.endsWith(etatBoxX) && allCases[3].src.endsWith(etatBoxX) && allCases[6].src.endsWith(etatBoxX)) ||
-//         (allCases[1].src.endsWith(etatBoxX) && allCases[4].src.endsWith(etatBoxX) && allCases[7].src.endsWith(etatBoxX)) ||
-//         (allCases[2].src.endsWith(etatBoxX) && allCases[5].src.endsWith(etatBoxX) && allCases[8].src.endsWith(etatBoxX)) ||
-//         // Lignes
-//         (allCases[0].src.endsWith(etatBoxX) && allCases[1].src.endsWith(etatBoxX) && allCases[2].src.endsWith(etatBoxX)) ||
-//         (allCases[3].src.endsWith(etatBoxX) && allCases[4].src.endsWith(etatBoxX) && allCases[5].src.endsWith(etatBoxX)) ||
-//         (allCases[6].src.endsWith(etatBoxX) && allCases[7].src.endsWith(etatBoxX) && allCases[8].src.endsWith(etatBoxX)) ||
-//         // Diagonales
-//         (allCases[0].src.endsWith(etatBoxX) && allCases[4].src.endsWith(etatBoxX) && allCases[8].src.endsWith(etatBoxX)) ||
-//         (allCases[2].src.endsWith(etatBoxX) && allCases[4].src.endsWith(etatBoxX) && allCases[6].src.endsWith(etatBoxX))
-//     ) {
-//         winner.src = etatBoxX;
-//         end();
-//     } else if (allCases.every(equality)) {
-//         textFinal.innerHTML = "Egalité !";
-//         end();
-//     }
-// }
-//
-// function equality(i) {
-//     return !i.src.endsWith(vide);
-// }
+function equality() {
+    let x = event.target.dataset.x;
+    let y = event.target.dataset.y;
+    for (x = 0; x < matrice.length; x++) {
+        (y = 0; y < matrice.length; y++) {
+            matrice[x][y] != 0;
+        }
+    }
+}
 
 // Fin du jeu
 
@@ -149,6 +110,11 @@ function reset() {
     modalBox.style.display = 'none';
     grid.style.cursor = 'pointer';
     grid.style.pointerEvents = '';
+    matrice = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
 }
 
 function animation(caseX) {
